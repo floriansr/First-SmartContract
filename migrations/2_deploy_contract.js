5f3e9a1405bd2d4d8776d85073a0a61b5d11bb3d
floriansr <sueurfloriansf@gmail.com>
2020-10-07 23:03:40 +0200

    [add] deploy_contract migration file

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -2,6 +2,6 @@ const HelloWorld = artifacts.require("HelloWorld");
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld);
   deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);
 };
