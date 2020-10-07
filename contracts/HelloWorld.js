c7fe147501e84876eb80f72acf97dff72c7cea00
floriansr <sueurfloriansf@gmail.com>
2020-10-07 16:25:46 +0200

    [add] HelloWorld contract

---
+++ contracts/HelloWorld.js
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract HelloWorld {
+  string public sentence;
+
+  constructor() public {
+    sentence = "HelloWorld !";
+  }
+
+  function saySomething() public view returns(string memory){
+    return (sentence);
+  }
+}
