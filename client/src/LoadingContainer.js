8be3aa728546369ce9894af22676283789d67d9d
floriansr <sueurfloriansf@gmail.com>
2020-10-08 23:32:17 +0200

    [add] drizzle loader

---
+++ client/src/LoadingContainer.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {drizzleReactHooks } from "@drizzle/react-plugin"
+
+const { useDrizzleState } = drizzleReactHooks
+
+const LoadingContainer = ({ children }) => {
+  const drizzleStatus = useDrizzleState((state) => state.drizzleStatus);
+  if (drizzleStatus.initialized === false) {
+    return "Loading...";
+  }
+  return <>{children}</>;
+};
+export default LoadingContainer
