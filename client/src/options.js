c1e2440ebab8a5030e7b63b00125c5c929eaf2b0
floriansr <sueurfloriansf@gmail.com>
2020-10-09 15:29:53 +0200

    [delete] web3 options

--- client/src/options.js
+++ client/src/options.js
@@ -2,12 +2,6 @@ import HelloWorld from "./contracts/HelloWorld.json";
 
 const options = {
   contracts: [HelloWorld],
-  web3: {
-    fallback: {
-      type: "ws",
-      url: "ws://127.0.0.1:7545",
-    },
-  },
 };
 
 export default options
